b656eef85a72bf7e2d3a33dccd9b38c35a74505d
Tamera Brown <Tam@Tameras-MBP.fios-router.home>
2022-05-19 00:04:33 -0400

    added user model to mongodb

---
+++ server/models/user.js
@@ -0,0 +1,12 @@
+const mogoose=require('mogoose')
+
+const UserSchema=mogoose.Schema({
+    firstName:{type:String,maxlength:50},
+    lastName:{type:String,maxlength:50},
+    email:{type:String,unique:1,trim:true},
+    password:{type:String,minlength:6}
+})
+const User=mogoose.model('users',UserSchema)
+
+module.exports={User}
+
